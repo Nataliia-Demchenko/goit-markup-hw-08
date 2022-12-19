7b46cd062ac0aec3d1bd78e2da1d8da3407057a2
Nataliia-Demchenko <113938605+Nataliia-Demchenko@users.noreply.github.com>
2022-12-19 06:57:29 +0200

    Finish

---
+++ js/mobile-menu.js
@@ -0,0 +1,25 @@
+(() => {
+  const mobileMenu = document.querySelector("[data-menu-container]");
+  const openMenuBtn = document.querySelector("[data-open-menu]");
+  const closeMenuBtn = document.querySelector("[data-close-menu]");
+  const wrapper = document.querySelector("body");
+
+  const toggleMenu = () => {
+    const isMenuOpen = openMenuBtn.getAttribute("aria-expanded") === "true" || false;
+    openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
+    mobileMenu.classList.toggle("is-open");
+
+    wrapper.classList.toggle("lock");
+  };
+
+  openMenuBtn.addEventListener("click", toggleMenu);
+  closeMenuBtn.addEventListener("click", toggleMenu);
+
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove("is-open");
+    openMenuBtn.setAttribute("aria-expanded", false);
+    wrapper.classList.remove("lock");
+  });
+})();
